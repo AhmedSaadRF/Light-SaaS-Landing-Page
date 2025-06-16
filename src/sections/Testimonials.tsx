29d239517be7251c0de2cdd204d1b84775611008
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-16 18:10:47 +0300

    Upload Project
    Add Style Classes

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -72,37 +72,37 @@ const secondColumn = testimonials.slice(3, 6)
 const thirdColumn = testimonials.slice(6, 9)
 
 const TestimonialsColumn = (props: { className?: string; testimonials: typeof testimonials }) => (
-  <div>
+  <div className={twMerge("flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]", props.className)}>
     {props.testimonials.map(({ text, imageSrc, name, username }) => (
-      <div key={text}>
+      <div key={text} className="card">
         <div>{text}</div>
-        <div>
-          <Image src={imageSrc} alt={name} width={40} height={40} />
-          <div>
-            <div>{name}</div>
-            <div>{username}</div>
+        <div className="flex items-center gap-2 mt-5">
+          <Image src={imageSrc} alt={name} width={40} height={40} className="h-10 w-10 rounded-full" />
+          <div className="flex flex-col">
+            <div className="font-medium tracking-tight leading-5">{name}</div>
+            <div className="leading-5 tracking-tight">{username}</div>
           </div>
         </div>
       </div>
     ))}
   </div>
-);
+)
 
 export const Testimonials = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <div>
-            <div></div>
+    <section className="bg-white">
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag"></div>
           </div>
-          <h2>What our users say</h2>
-          <p>From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
+          <h2 className="section-title mt-5">What our users say</h2>
+          <p className="section-description mt-5">From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
         </div>
-        <div>
+        <div className="flex justify-center gap-6">
           <TestimonialsColumn testimonials={firstColumn} />
-          <TestimonialsColumn testimonials={secondColumn} />
-          <TestimonialsColumn testimonials={thirdColumn} />
+          <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex" />
+          <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex" />
         </div>
       </div>
     </section>
