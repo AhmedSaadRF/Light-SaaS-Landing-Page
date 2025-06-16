91e53433a36187fd1da62ed86a9c8b5c3f3d77c9
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-16 15:37:17 +0300

    Upload Project
    Finish Product Show Case Section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,25 @@
+import productImage from '@/assets/product-image.png'
+import pyramidImage from '@/assets/pyramid.png'
+import tubeImage from '@/assets/tube.png'
+import Image from "next/image";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section>
+      <div>
+        <div>
+          <div>
+            <div>Boost your productivity</div>
+          </div>
+          <h2>A more effective way to track progress</h2>
+          <p>Effortlessly turn your ideas into a fully functional, responsive, Saas website in just minutes with this template.</p>
+        </div>
+        <div>
+          <Image src={productImage} alt='Product Image' />
+          <Image src={pyramidImage} alt='pyramid Image' height={262} width={262} />
+          <Image src={tubeImage} alt='Tube Image' height={248} />
+        </div>
+      </div>
+    </section>
+  );
 };
