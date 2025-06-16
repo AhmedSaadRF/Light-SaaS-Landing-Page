8ff5bf4d009a20ee535c928b9eaa6a084ef0a671
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-16 19:06:35 +0300

    Upload Project
    Add Style Classes

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -5,19 +5,19 @@ import Image from 'next/image';
 
 export const CallToAction = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <h2>Sign up for free today</h2>
-          <p>Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
-          <Image src={starImage} alt='Star Image' width={360} />
-          <Image src={springImage} alt='Spring Image' width={360} />
+    <section className='bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip'>
+      <div className="container">
+        <div className='section-heading relative'>
+          <h2 className='section-title'>Sign up for free today</h2>
+          <p className='section-description mt-5'>Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
+          <Image src={starImage} alt='Star Image' width={360} className='absolute -left-[350px] -top-[137px]' />
+          <Image src={springImage} alt='Spring Image' width={360} className='absolute -right-[331px] -top-[19px]' />
         </div>
-        <div>
-          <button>Get for free</button>
-          <button>
+        <div className='flex gap-2 mt-10 justify-center'>
+          <button className='btn btn-primary'>Get for free</button>
+          <button className='btn btn-text gap-1'>
             <span>Learn more</span>
-            <ArrowRight />
+            <ArrowRight className="h-5 w-5" />
           </button>
         </div>
       </div>
